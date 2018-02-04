e724c8e5c6226c576701bf190df5fd64c2eb89c4
Eli Selkin <ejselkin@cpp.edu>
2018-02-03 17:56:30 -0800

    Now this is actually more like the little subwindows in Azure

--- azure.js
+++ azure.js
@@ -1,47 +1,56 @@
-// 'esversion: 6'
+// jshint esversion: 6
+let hoverOver = [];
 $(document).ready(function() {
-  console.log("ready");
-  $(".subWindow").mouseenter(hoverOverSubWindow);
+    $(".subWindow").mouseenter(hoverOverSubWindow);
 });
 
 function hoverOverSubWindow(evt) {
-  let h = evt.target.clientHeight;
-  let w = evt.target.clientWidth;
-  let id = evt.target.id;
-  $("#" + id).wrap(
-    '<div id="' +
-      id +
-      "w" +
-      '" class="wrapperSubWindow" style="width:' +
-      parseFloat(w) +
-      "; height:" +
-      (parseFloat(h) + 75) +
-      ';"></div>'
-  );
-  $("#" + id + "w").width(parseFloat(w));
-  $("#" + id + "w").height(parseFloat(h));
-  $("#" + id).unbind();
-  $("#" + id + "w").mouseleave(hoverOutSubWindow);
-  $("#" + id + "t").mouseleave(hoverOutSubWindow);
-  $("#" + id).before(
-    '<div id="' +
-      id +
-      "t" +
-      '" class="titleBar"><div class="xRemove">X</div></div>'
-  );
+    let h = evt.target.clientHeight;
+    let w = evt.target.clientWidth;
+    let t = evt.target.offsetTop;
+    let l = evt.target.offsetLeft;
+    let id = evt.target.id;
+    if (
+        $(`#${id}-c`).length === 0 &&
+        !(id.substr(id.length - 2) in ["-t", "-w", "-c"])
+    ) {
+        let clone = $(`#${id}`).clone();
+        clone.attr("id", `${id}-c`);
+        clone.css("z-index", 100);
+        $(`#${id}`).after(clone);
+        $(`#${id}-c`).wrap(`<div id="${id}-w" class="subWindowWraper"></div>`);
+        $(`#${id}-w`).unbind();
+        $(`#${id}-w`).mouseleave(hoverOutSubWindow);
+        // empty div to take spot so other things don't shift around
+        $(`#${id}-c`).before(
+            `<div id="${id}-t" class="titleBar"><div class="xRemove">X</div></div>`
+        );
+        $().width(parseFloat(w));
+        $(`#${id}-w`).height(parseFloat(h) + 40);
+        $(`#${id}-w`).offset({ top: parseFloat(t) - 40, left: l });
+        $(`#${id}-c`).removeClass("subWindow");
+        $(`#${id}-c`).addClass("subWindowInWrapper");
+        $(`#${id}`).unbind();
+        $(`#${id}-t`).unbind();
+    }
 }
 
 function hoverOutSubWindow(evt) {
-  let id = evt.target.id;
-  // remove element before and unwrap and reallow mouseenter!
-  if (id.substr(id.length - 1) === "t" || id.substr(id.length - 1) === "w") {
-    id = id.substr(0, id.length - 1);
-  }
-  if (id === "") {
-    return;
-  }
-  $("#" + id).unwrap();
-  $("#" + id + "t").remove();
-  $("#" + id).unbind();
-  $("#" + id).mouseenter(hoverOverSubWindow);
+    let id = evt.target.id;
+    // remove element before and unwrap and reallow mouseenter!
+    if (
+        id.substr(id.length - 2) === "-t" ||
+        id.substr(id.length - 2) === "-w" ||
+        id.substr(id.length - 2) === "-c"
+    ) {
+        id = id.substr(0, id.length - 2);
+    }
+    if (id === "") {
+        return;
+    }
+    $(`#${id}-t`).remove();
+    $(`#${id}-c`).remove();
+    $(`#${id}-w`).remove();
+    $("#" + id).unbind();
+    $("#" + id).mouseenter(hoverOverSubWindow);
 }
