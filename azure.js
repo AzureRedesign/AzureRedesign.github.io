cab7f48bba144d24719ead473af81d021af6c1b5
Eli Selkin <ejselkin@cpp.edu>
2018-02-03 19:01:30 -0800

    better x performance :-p

--- azure.js
+++ azure.js
@@ -1,5 +1,4 @@
 // jshint esversion: 6
-let hoverOver = [];
 $(document).ready(function() {
     $(".subWindow").mouseenter(hoverOverSubWindow);
 });
@@ -10,10 +9,7 @@ function hoverOverSubWindow(evt) {
     let t = evt.target.offsetTop;
     let l = evt.target.offsetLeft;
     let id = evt.target.id;
-    if (
-        $(`#${id}-c`).length === 0 &&
-        !(id.substr(id.length - 2) in ["-t", "-w", "-c"])
-    ) {
+    if (!(id.substr(id.length - 2) in ["-t", "-w", "-c", "-x"])) {
         $(".subWindow").each(index => {
             // get out of each other subwindow that's not a match, sometimes this gets missed
             if (this.id !== id && $(this.id + "-w").length !== 0) {
@@ -24,20 +20,21 @@ function hoverOverSubWindow(evt) {
         clone.attr("id", `${id}-c`);
         clone.css("z-index", 100);
         $(`#${id}`).after(clone);
+        $(`#${id}-c`).removeClass("subWindow");
+        $(`#${id}-c`).addClass("subWindowInWrapper");
         $(`#${id}-c`).wrap(`<div id="${id}-w" class="subWindowWraper"></div>`);
         $(`#${id}-w`).unbind();
         $(`#${id}-w`).mouseleave(hoverOutSubWindow);
         // empty div to take spot so other things don't shift around
-        $(`#${id}-c`).before(
-            `<div id="${id}-t" class="titleBar"><div id="${id}-x" class="xRemove">X</div></div>`
-        );
-        $().width(parseFloat(w));
+        $(`#${id}-c`).before(`<div id="${id}-t" class="titleBar"></div>`);
+        $(`#${id}-t`).append(`<div id="${id}-x" class="xRemove">X</div>`);
+        $(`#${id}-x`).unbind();
+        // $(`#${id}-x`).mouseleave(hoverOutSubWindow);
         $(`#${id}-w`).height(parseFloat(h) + 30);
         $(`#${id}-w`).offset({ top: parseFloat(t) - 30, left: l });
         $(`#${id}-x`).on("click", removeSubWindow);
-        $(`#${id}-c`).removeClass("subWindow");
-        $(`#${id}-c`).addClass("subWindowInWrapper");
         $(`#${id}`).unbind();
+        $(`#${id}-c`).unbind();
         $(`#${id}-t`).unbind();
     }
 }
@@ -45,7 +42,6 @@ function hoverOverSubWindow(evt) {
 function removeSubWindow(evt) {
     let id = evt.target.id;
     id = id.substr(0, id.length - 2); // root object
-    console.log(id);
     $(`#${id}-w`).trigger("mouseout");
     $(`#${id}`).remove();
 }
@@ -56,7 +52,8 @@ function hoverOutSubWindow(evt) {
     if (
         id.substr(id.length - 2) === "-t" ||
         id.substr(id.length - 2) === "-w" ||
-        id.substr(id.length - 2) === "-c"
+        id.substr(id.length - 2) === "-c" ||
+        id.substr(id.length - 2) === "-x"
     ) {
         id = id.substr(0, id.length - 2);
     }
@@ -66,6 +63,7 @@ function hoverOutSubWindow(evt) {
     $(`#${id}-t`).remove();
     $(`#${id}-c`).remove();
     $(`#${id}-w`).remove();
+    $(`#${id}-x`).remove();
     $("#" + id).unbind();
     $("#" + id).mouseenter(hoverOverSubWindow);
 }
